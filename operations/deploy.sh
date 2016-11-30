d9ee2aa1ffdec42cfaf66561f7eea22f19c3143e
kevin olson <acidjazz@gmail.com>
2016-11-29 19:47:39 -0800

    pub to public

--- operations/deploy.sh
+++ operations/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3designsake sync pub/. s3://goldpr.com --exclude '.DS_Store'
+s3cmd --config=/Users/k/.s3designsake sync public/ s3://goldpr.com --exclude '.DS_Store'
 s3cmd setacl --config=/Users/k/.s3designsake s3://goldpr.com/ --acl-public --recursive
