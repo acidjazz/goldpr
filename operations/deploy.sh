70a1627302238002db989917ddae2ac064f40276
kevin olson <acidjazz@gmail.com>
2017-01-20 13:18:21 -0800

    moving s3

--- operations/deploy.sh
+++ operations/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3designsake sync public/ s3://goldpr.com --exclude '.DS_Store'
-s3cmd setacl --config=/Users/k/.s3designsake s3://goldpr.com/ --acl-public --recursive
+s3cmd --config=/Users/k/.s3/.s3designsake sync public/ s3://goldpr.com --exclude '.DS_Store'
+s3cmd setacl --config=/Users/k/.s3/.s3designsake s3://goldpr.com/ --acl-public --recursive
